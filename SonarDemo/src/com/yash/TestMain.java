d30259bf08304b4b95d2d97249d5a4260f946b7c
shyam.patidar <shyam.patidar@YI1007113DT.Yash.com>
2018-05-28 15:50:26 +0530

    sonar demo

---
+++ SonarDemo/src/com/yash/TestMain.java
@@ -0,0 +1,12 @@
+package com.yash;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20;
+		
+		System.out.println(b);
+	}
+
+}
