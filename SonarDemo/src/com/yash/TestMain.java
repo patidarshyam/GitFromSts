a8884f16ad41a7cfd282c23a50c63591961cc532
patidarshyam <34019866+patidarshyam@users.noreply.github.com>
2018-05-28 15:52:32 +0530

    Update TestMain.java

--- SonarDemo/src/com/yash/TestMain.java
+++ SonarDemo/src/com/yash/TestMain.java
@@ -5,7 +5,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10,b=20;
-		
+		// Test ................
 		System.out.println(b);
 	}
 
